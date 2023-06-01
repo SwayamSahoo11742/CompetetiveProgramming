052dafd509876fe4229cf4f8361e1caae88543ea
SwayamSahoo11742 <swayamsa01@gmail.com>
2023-05-31 21:53:46 -0400

    05/31/23

--- CODEFORCES/1200/1737B.cpp
+++ CODEFORCES/1200/1737B.cpp
@@ -1,9 +1,16 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int main(){
-    int T; cin >> T;
-    while(T--){
-        int l,r; cin >> l >> r;
-        
-    }
+#define ll long long 
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	ll a,b;
+	cin>>a>>b;
+	ll l=sqrt(1.0L*a),r=sqrt(1.0L*b);
+	ll ans=(r-l)*3+(b-r*r)/r-(a-l*l)/l+((a-l*l)%l==0);
+	cout<<ans<<endl;
+	}
 }
